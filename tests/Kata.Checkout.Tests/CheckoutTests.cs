d3370acca0215b633305beccf5aaf3201f0641f1
Aktar Hussain <Aktar.Hussain@travelcounsellors.com>
2020-03-25 17:15:30 +0000

    Intial setup of solution.  Added .netcore class library and test project for Checkout

---
+++ tests/Kata.Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Checkout.Tests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
